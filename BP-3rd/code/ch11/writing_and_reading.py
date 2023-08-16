8d512b122b3c702bb26621598e8b1242ec9cfbe1
sunyardCUI <1924375084@qq.com>
2023-08-16 17:29:45 +0800

    文件读写

---
+++ BP-3rd/code/ch11/writing_and_reading.py
@@ -0,0 +1,11 @@
+"""
+@Description: 读取和写入
+@Author(s): Stephen CUI
+@LastEditor(s): Stephen CUI
+@CreatedTime: 2023-08-16 17:26:36
+"""
+
+f = open('somefile.txt', 'w')
+f.write("Hello, ")
+f.write("World!")
+f.close()
