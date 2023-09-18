14ae564b7f6f429128e004b086b77c56ec190b2c
Stephen CUI <junocuixuan@163.com>
2023-09-18 23:12:14 +0800

    多进程与多线程计算 \pi

---
+++ HPPY-2nd/code/ch09_the_multiprocessing_module/01_pi_lists_parallel.cmd
@@ -0,0 +1,9 @@
+python 01_pi_lists_parallel.py --nbr_samples_in_total 100000000 --processes 1
+python 01_pi_lists_parallel.py --nbr_samples_in_total 100000000 --processes 2
+python 01_pi_lists_parallel.py --nbr_samples_in_total 100000000 --processes 4
+python 01_pi_lists_parallel.py --nbr_samples_in_total 100000000 --processes 8
+
+python 01_pi_lists_parallel.py --nbr_samples_in_total 100000000 1
+python 01_pi_lists_parallel.py --nbr_samples_in_total 100000000 2
+python 01_pi_lists_parallel.py --nbr_samples_in_total 100000000 4
+python 01_pi_lists_parallel.py --nbr_samples_in_total 100000000 8
