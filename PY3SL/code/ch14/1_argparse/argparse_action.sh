5fd9dfdbba653bce5690922144fa75df49469e98
Stephen CUI <junocuixuan@163.com>
2023-12-04 21:46:16 +0800

    参数动作

---
+++ PY3SL/code/ch14/1_argparse/argparse_action.sh
@@ -0,0 +1,15 @@
+python argparse_action.py -h
+
+python argparse_action.py -s value
+
+python argparse_action.py -c
+
+python argparse_action.py -t
+
+python argparse_action.py -f
+
+python argparse_action.py -a one -a two -a 3
+
+python argparse_action.py -B -A
+
+python argparse_action.py --version
