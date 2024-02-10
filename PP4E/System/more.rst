790d360fc67566e65eccce58917203316e3aa211
Stephen CUI <junocuixuan@163.com>
2024-02-11 00:23:54 +0800

    系统编程

---
+++ PP4E/System/more.rst
@@ -0,0 +1,6 @@
+>>> line = 'aaa\nbbb\nccc\n'
+>>> line.split('\n')
+['aaa', 'bbb', 'ccc', '']
+>>> line.splitlines()
+['aaa', 'bbb', 'ccc']
+>>>
---
+++ PP4E/System/notes.py
@@ -0,0 +1,19 @@
+"""
+@File         : nots.py
+@Author(s)    : Stephen CUI
+@LastEditor(s): Stephen CUI
+@CreatedTime  : 2024-02-10 23:54:33
+@Email        : cuixuanstephen@gmail.com
+@Description  : 
+"""
+
+import streamlit as st
+
+st.title("系统工具")
+st.header("系统编程概述")
+st.subheader("字符串方法基本知识")
+st.write(
+    """
+    `find` 函数返回子字符串第一个匹配所在的偏移位置，`replace` 函数则是完全全局搜索和替换。
+    """
+)
