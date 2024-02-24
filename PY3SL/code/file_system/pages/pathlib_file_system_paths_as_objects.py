eeff511ae39be2affbead2cba11f8e31e83b1ac2
Stephen CUI <junocuixuan@163.com>
2024-02-24 15:08:00 +0800

    目录内容

--- PY3SL/code/file_system/pages/pathlib_file_system_paths_as_objects.py
+++ PY3SL/code/file_system/pages/pathlib_file_system_paths_as_objects.py
@@ -109,3 +109,124 @@ with tabs[1]:
         ```
     """
     )
+
+with tabs[2]:
+    st.markdown(
+        """
+    ```python
+    >>> import pathlib
+    >>>
+    ```
+    路径对象具有用于从名称中提取部分值的方法和属性。 例如，`parts` 属性生成基于路径分隔符值解析的路径段序列。
+    
+    
+    ```python
+    >>> p = pathlib.PurePosixPath("/usr/local")
+    >>> print(p.parts)
+    ('/', 'usr', 'local')
+    >>>
+    ```
+    
+    有两种方法可以从给定路径对象“向上”导航文件系统层次结构。 `parent` 属性引用包含路径（该值由 `os.path.dirname()` 返回）的目录的新路径实例，。 `parents` 属性是一个可迭代对象，它生成父目录引用，不断“向上”路径层次结构，直到到达根目录。
+    
+    ```python
+    >>> p = pathlib.PurePosixPath("/usr/local/lib")
+    >>>
+    >>> print(f"parent: {p.parent}")
+    parent: /usr/local
+    >>> print("Hierarchy:")
+    Hierarchy:
+    >>> for up in p.parents:
+    ...     print(up)
+    ... 
+    /usr/local
+    /usr
+    /
+    >>>
+    ```
+    路径的其他部分可以通过路径对象的属性来访问。`name` 属性保存路径的最后部分，位于最终路径分隔符之后（与 `os.path.basename()` 生成的值相同）。`suffix` 属性保存扩展名分隔符之后的值，`stem` 属性保存后缀之前的名称部分。
+    
+    ```python
+    >>> p = pathlib.PurePosixPath("./source/pathlib/pathlib_name.py")
+    >>>
+    >>> print(f"path   : {p}")
+    path   : source/pathlib/pathlib_name.py
+    >>> print(f"name   : {p.name}")
+    name   : pathlib_name.py
+    >>> print(f"suffix : {p.suffix}")
+    suffix : .py
+    >>> print(f"stem   : {p.stem}")
+    stem   : pathlib_name
+    >>>
+    ```
+    """
+    )
+
+with tabs[3]:
+    st.markdown(
+        """
+    ```python
+    >>> import pathlib
+    >>>
+    ```
+    具体 `Path` 类的实例可以从引用文件系统上的文件、目录或符号链接的名称（或潜在名称）的字符串参数创建。
+    
+    ```python
+    >>> home = pathlib.Path.home()
+    >>> print("home:", home)
+    home: C:\\Users\\JPL-JUNO
+    >>> cwd = pathlib.Path.cwd()
+    >>> print("cwd:", cwd)
+    cwd: D:\\Python-3\\PY3SL
+    >>>
+    ```
+    """
+    )
+
+with tabs[4]:
+    st.markdown(
+        """
+    ```python
+    >>> import pathlib
+    >>>
+    ```
+    
+    可以使用三种方法来访问目录列表并发现文件系统上可用的文件名称。`iterdir()` 是一个生成器，为包含目录中的每个项目生成一个新的 `Path` 实例。
+    
+    ```python
+    >>> import pathlib
+    >>> p = pathlib.Path(".")
+    >>> for f in p.iterdir():
+    ...     print(f)
+    ... 
+    ospath_abspath.py
+    ospath_basename.py
+    ospath_commonpath.py
+    ospath_commonprefix.py
+    -- snipped --
+    ```
+    如果 `Path` 不表示目录，`iterdir()` 会引发 `NotADirectoryError`。
+    
+    使用 `glob()` 仅查找与模式匹配的文件。
+    ```python
+    >>> p = pathlib.Path(".")
+    >>> for f in p.glob("*.rst"):
+    ...     print(f)
+    ...
+    >>>
+    ```
+    `glob` 处理器支持使用模式前缀 `**` 或通过调用 `rglob()` 而不是 `glob()` 进行递归扫描。
+    ```python
+    >>> p = pathlib.Path("../")
+    >>> for f in p.rglob("*.py"):
+    ...     print(f)
+    ... 
+    ..\\file_system.py
+    ..\\pages\\os.path_platform-independent_manipulation_of_filenames.py
+    ..\\pages\\pathlib_file_system_paths_as_objects.py
+    ..\\source\\ospath_abspath.py
+    ..\\source\\ospath_basename.py
+    -- snipped
+    ```
+    """
+    )
